7e5d9b7785261ee8f3bf826b37bcd73399df3e48
Sean D. Hunt <seandhunt_7@yahoo.com>
2018-04-15 19:46:58 -0400

    Start with keyboard and mouse processing.

---
+++ src/kernel/keyboard.c
@@ -0,0 +1,7 @@
+/*
+ * Keyboard OS Driver.
+ */
+#define WITH_KERNEL
+#include "../../include/Cyanyde.h"
+
+// TODO: Add kernel function that makes the os able to process all keyboard inputs for all processes.
