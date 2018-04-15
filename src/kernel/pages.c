91416884001b666d6e907f0911121aa9452569bc
Sean D. Hunt <seandhunt_7@yahoo.com>
2018-04-15 01:33:24 -0400

    Implement paging hopefully.

--- src/kernel/pages.c
+++ src/kernel/pages.c
@@ -10,6 +10,20 @@ PAGEDATALIST MakePages(void[] data)
 {
   // make pages by every 4096
   // bytes in the array.
+  int page_num = 0;
   PAGEDATALIST pagelist;
+  for (int page = 0; page < sizeof(data); page + 4096)
+  {
+    PAGEDATA pdata;
+    for (int i = 0; i < page; i++)
+    {
+      pdata.data[i] = data[i];
+      if (i == page)
+      {
+        page_num++;
+        pagelist.page[page_num] = pdata;
+      }
+    }
+  }
   return pagelist;
 }
