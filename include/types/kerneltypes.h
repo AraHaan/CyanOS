afa2f38bfb8c69bb7d9234938880c3455a335842
AraHaan <seandhunt_7@yahoo.com>
2018-10-28 20:55:32 -0400

    Include forgotten include file.

---
+++ include/types/kerneltypes.h
@@ -0,0 +1,24 @@
+/*
+ * types/kerneltypes.h
+ * Defines standard types used by the OS.
+ */
+#ifndef TYPES_KERNELTYPES_H
+#define TYPES_KERNELTYPES_H
+
+
+typedef char int8_t;
+typedef unsigned char uint8_t;
+
+typedef short int16_t;
+typedef unsigned short uint16_t;
+
+typedef int int32_t;
+typedef unsigned int uint32_t;
+
+typedef long long int int64_t;
+typedef unsigned long long int uint64_t;
+
+typedef const char* string;
+
+
+#endif
